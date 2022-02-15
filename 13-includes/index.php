b7f6a1808fe3f66f1f73773a5f1c5008ebbf5727
Miguel Angel Cornielly H <mcornielly@gmail.com>
2022-02-15 17:21:19 -0400

    add include topic course master php

---
+++ 13-includes/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include 'includes/headers.php'; ?>
+    <hr>
+    <!-- Content -->
+    <div class="content">
+        <h2>Esta es la pagina de inicio</h2>
+        <p>texto de prueba de la pagina de inicio</p>
+    </div>
+
+    <!-- Footer -->
+    <hr>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
